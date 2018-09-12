72390ce16040809562f1ccd9980ce7eb2666f8b6
zhengjianhui <zhengjianhui@yunzhangfang.com>
2018-09-12 16:23:07 +0800

    add swagger config

---
+++ project-web/src/main/java/com/yzf/project/projectweb/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.yzf.project.projectweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author zhengjianhui on 18/3/16
+ */
+@Configuration
+@EnableSwagger2
+@Profile({"dev", "test"})
+public class SwaggerConfig {
+
+    @Bean
+    public Docket testApi() {
+
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.yzf.project.projectweb.controller")).paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("project").description("project").termsOfServiceUrl("").contact(new Contact("zhengjianhui ", "", "")).version("1.0").build();
+    }
+
+}
