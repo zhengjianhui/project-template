8447706e60c416670b97e6378d93487f3faa10ce
zhengjianhui <zhengjianhui@yunzhangfang.com>
2018-09-12 11:43:35 +0800

    admin template

---
+++ project-admin/src/main/java/com/yzf/project/projectadmin/ProjectAdminApplication.java
@@ -0,0 +1,12 @@
+package com.yzf.project.projectadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAdminApplication.class, args);
+	}
+}
