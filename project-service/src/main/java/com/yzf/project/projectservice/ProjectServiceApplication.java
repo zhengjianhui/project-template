6bfd1fdc9910f1ae1797775c7fbe37b3cd820468
zhengjianhui <zhengjianhui@yunzhangfang.com>
2018-09-12 11:47:42 +0800

    init service template

---
+++ project-service/src/main/java/com/yzf/project/projectservice/ProjectServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yzf.project.projectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+}
---
+++ project-service/src/test/java/com/yzf/project/projectservice/ProjectServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yzf.project.projectservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
