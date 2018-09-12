3fbba086af7f6df5ac5b34bfc31cd6bfe51e994a
zhengjianhui <zhengjianhui@yunzhangfang.com>
2018-09-12 11:45:54 +0800

    init web template

---
+++ project-web/src/main/java/com/yzf/project/projectweb/ProjectWebApplication.java
@@ -0,0 +1,12 @@
+package com.yzf.project.projectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebApplication.class, args);
+	}
+}
